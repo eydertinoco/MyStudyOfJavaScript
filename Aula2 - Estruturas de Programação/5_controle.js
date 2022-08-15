2ddf2c87221b7dde77a9f1954ba515c4b4022634
Eyder <eydertinoco@outlook.com>
2022-08-15 19:34:45 -0300

    - Finalizado segunda aula;

---
+++ Aula2 - Estruturas de Programação/5_controle.js
@@ -0,0 +1,15 @@
+// switch/case
+
+let nome = "Eyder";
+
+switch(nome) {
+    case "João":
+        console.log(`O nome é ${nome} é um usuário`);
+        break;
+    case "Eyder":
+        console.log(`O nome é ${nome} é um Administrador`);
+        break;
+    default:
+        console.log(`Não não encontrado`);
+        break;
+}
