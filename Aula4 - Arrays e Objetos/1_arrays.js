4092f6bf6302222b5a7750d57b5f1d75752d9719
Eyder <etinoco@indracompany.com>
2022-08-16 16:43:31 -0300

    - começando aula 4

---
+++ Aula4 - Arrays e Objetos/1_arrays.js
@@ -0,0 +1,11 @@
+let numb = [1, 4, 5, 6, 7, 8, 9];
+let text = ["João", 'Matheus', `Eyder`];
+let bool = [true, false, true];
+let misturado = [1, "joão", true];
+
+console.log(numb, text, bool, misturado);
+
+console.log(numb[3]);
+console.log(text[0]);
+console.log(bool[1]);
+console.log(numb[numb.length - 1]);
