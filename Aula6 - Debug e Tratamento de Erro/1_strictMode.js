752c2098f1794f4a69b67d83ceaefdb93e5db0db
Eyder <eydertinoco@outlook.com>
2022-08-22 20:08:35 -0300

    - Começando a aula 6

---
+++ Aula6 - Debug e Tratamento de Erro/1_strictMode.js
@@ -0,0 +1,10 @@
+// Ao digitar "use strict" no começo do codigo, ele força você a definir a variavel (var, let e etc);
+
+"use strict"
+
+let opa = 'teste';
+
+function teste() {
+    // Se usar "use strict" dentro da função, ele só funciona dentro da função.
+
+}
