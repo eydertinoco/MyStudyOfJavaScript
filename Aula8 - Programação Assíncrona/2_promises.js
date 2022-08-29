d473be205661fa3a3787410c50992e9167670170
Eyder <etinoco@indracompany.com>
2022-08-29 13:48:17 -0300

    - Aula 8

--- Aula8 - Programação Assíncrona/2_promises.js
+++ Aula8 - Programação Assíncrona/2_promises.js
@@ -2,3 +2,35 @@ let promessa = Promise.resolve(4 + 8);
 
 console.log('Algum Código');
 promessa.then((value) => console.log(`A soma é ${value}`));
+
+// Falha nas promises
+// Exibir o erro no console
+let promiseErrada = Promise.resolve(new Error("Algo deu errado"));
+promiseErrada
+    .then((value) =>console.log(value))
+    .catch(reason => console.log("Falha: " + reason));
+
+// Rejeitando Promises
+function verificarAlgo(num)  {
+    return new Promise((resolve, reject) => {
+        if(num == 2) {
+            resolve(console.log("O número é 2"));
+        } else {
+            reject(new Error("Valor não é 2"));
+        }
+    })
+}
+verificarAlgo(2);
+// verificarAlgo(3);
+
+// Resolvendo várias Promises
+const p1 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve(100);
+    }, 2500);
+})
+const p2 = Promise.resolve(5);
+const p3 = new Promise(function (resolve, reject) {
+    resolve(10);
+});
+Promise.all([p1, p2, p3]).then((values) => console.log(values));
