afbbbc54566fd7fc1e7d47dd66119a9517e4a3a8
Eyder <etinoco@indracompany.com>
2022-09-12 14:33:01 -0300

    - Aula 9

---
+++ Aula9 - DOM/1_encontrandoElementos.js
@@ -0,0 +1,12 @@
+// document = DOM
+
+let titulo = document.getElementsByTagName("h1")
+console.log(titulo);
+
+console.log(document.getElementById("myId"));
+
+console.log(document.getElementsByClassName('myClass'));
+
+console.log(document.querySelector('h1'));
+console.log(document.querySelector('#myId'));
+console.log(document.querySelector('.myClass'));
